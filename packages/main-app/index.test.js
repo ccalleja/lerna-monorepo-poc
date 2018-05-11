36ccfc642755ed8535ec62b948b923229fdec118
Clint Calleja <clint.calleja@tipico.com>
2018-05-11 13:57:22 +0200

    Splitting unit and integration tests

--- packages/main-app/index.test.js
+++ packages/main-app/index.test.js
@@ -4,10 +4,6 @@ const grocery = require("./");
 test("outputs correct string", () => {
     expect(log.mock.calls.length).toBe(3);
     expect(log.mock.calls[0][0]).toBe("Main App and TEST");
-    // mocked
-    /*expect(log.mock.calls[1][0]).toBe("TEST");
-    expect(log.mock.calls[2][0]).toBe("TEST");*/
-    // integrated
-    expect(log.mock.calls[1][0]).toBe("Package A and TEST");
-    expect(log.mock.calls[2][0]).toBe("Package B and TEST");
+    expect(log.mock.calls[1][0]).toBe("TEST");
+    expect(log.mock.calls[2][0]).toBe("TEST");
 });
