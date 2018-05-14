edb3975689b7d548752f37d1b133a25e356be0cc
Clint Calleja <clint.calleja@tipico.com>
2018-05-14 17:14:16 +0200

    Small change in 1 module

--- packages/main-app/index.js
+++ packages/main-app/index.js
@@ -1,6 +1,6 @@
 const sillyname = require('sillyname');
 const packageA = require('package-a');
 const packageB = require('package-b');
-console.log(`Main App and ${sillyname()}`);
+console.log(`Main App Test 2 and ${sillyname()}`);
 console.log(packageA);
 console.log(packageB);
