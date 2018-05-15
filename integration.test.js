1a95c5ce3916a0fcc23b6e6f20de4f68a877bfc7
Clint Calleja <clint.calleja@tipico.com>
2018-05-15 10:40:47 +0200

    Fix tests

--- integration.test.js
+++ integration.test.js
@@ -4,7 +4,7 @@ const log = jest.fn();
 console.log = log;
 const mainApp = require('./packages/main-app/');
 test('outputs correct string', () => {
-  expect(log.mock.calls.length).toBe(3);
+  expect(log.mock.calls.length).toBe(4);
   expect(log.mock.calls[0][0]).toBe('Main App Test 2 and TEST');
   expect(log.mock.calls[1][0]).toBe('Package A Test 2 and TEST');
   expect(log.mock.calls[2][0]).toBe('Package B and TEST');
--- packages/main-app/index.test.js
+++ packages/main-app/index.test.js
@@ -2,7 +2,7 @@ const log = jest.fn();
 console.log = log;
 const grocery = require("./");
 test("outputs correct string", () => {
-    expect(log.mock.calls.length).toBe(3);
+    expect(log.mock.calls.length).toBe(4);
     expect(log.mock.calls[0][0]).toBe("Main App Test 2 and TEST");
     expect(log.mock.calls[1][0]).toBe("TEST");
     expect(log.mock.calls[2][0]).toBe("TEST");
